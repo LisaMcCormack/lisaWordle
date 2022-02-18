9fff58b3e5a8a644b5bc7329514e6e9e6ba526c4
LMcCormack <lisa.mccormack@dunelm.com>
2022-02-18 13:55:34 +0000

    keyboard working

--- src/App.js
+++ src/App.js
@@ -1,23 +1,31 @@
-import React from 'react';
+import React, {useState} from 'react';
 import Keyboard from 'react-simple-keyboard';
 import 'react-simple-keyboard/build/css/index.css';
 import './App.css';
 
 function App() {
+    const [guesses, setGuesses] = useState("")
+
     const onChange = (input) => {
         console.log("Input changed", input);
     }
 
     const onKeyPress = (button) => {
-        console.log("Button pressed", button);
+        console.log('>>>', button)
+        if (button === '{bksp}') {
+            setGuesses(guesses.substring(0, guesses.length - 1))
+        } else {
+            setGuesses(guesses + button);
+        }
+        console.log('>>>', guesses);
     }
 
     return (
         <div className="App">
             <h1>Wordle</h1>
             <div className='guesses'>
-                {Array.from(Array(30), () =>
-                  <div className="box"/>
+                {Array.from(Array(30), (e, i) =>
+                    <div className="box" key={i}>{guesses[i]}</div>
                 )}
             </div>
             <div className="keyboard">
